c59066ee829be5b4636e9e1e0780820583559a63
Alan Marinho <kgometalenator@gmail.com>
2024-02-08 20:28:06 -0300

    websocket + final commit

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import {z} from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "crypto";
+import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 
 export async function voteOnPoll(app: FastifyInstance){
@@ -36,6 +38,14 @@ export async function voteOnPoll(app: FastifyInstance){
                         id: userPreviousVoteOnPoll.id
                     }
                 })
+
+                const votes = await redis.zincrby(pollId, -1,  userPreviousVoteOnPoll.pollOptionId) 
+                
+                voting.publish(pollId, {
+                    pollOptionId,
+                    votes: Number(votes),
+                })
+
             } else if (userPreviousVoteOnPoll) {
                 return reply.code(409).send({message:"You have already voted on this poll."}) 
             } 
@@ -59,6 +69,13 @@ export async function voteOnPoll(app: FastifyInstance){
                 pollId,
                 pollOptionId,
             }
+        })
+
+        const votes = await redis.zincrby(pollId,  1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
         })
 
         return reply.status(201).send();
